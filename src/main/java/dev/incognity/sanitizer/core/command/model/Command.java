9938da94de9137e68f882ba32a31c9932a2f83a0
zincognity <67128655+zincognity@users.noreply.github.com>
2026-01-02 00:31:02 -0500

    feat: Implement command feedback methods and tab completion utilities

--- src/main/java/dev/incognity/sanitizer/core/command/model/Command.java
+++ src/main/java/dev/incognity/sanitizer/core/command/model/Command.java
@@ -1,5 +1,7 @@
 package dev.incognity.sanitizer.core.command.model;
 
+import java.util.List;
+
 import javax.annotation.Nonnull;
 
 import org.bukkit.ChatColor;
@@ -37,5 +39,63 @@ public abstract class Command extends BukkitCommand {
   @Override
   public String getUsage() {
     return ChatColor.translateAlternateColorCodes('&', super.getUsage());
+  }
+
+  // Feedback methods
+  protected void sendSuccess(@Nonnull CommandSender sender, @Nonnull String message) {
+    CommandFeedback.success(sender, message);
+
+  }
+
+  protected void sendSuccess(@Nonnull CommandSender sender, @Nonnull String message,
+      @Nonnull String title) {
+    CommandFeedback.success(sender, message, title);
+  }
+
+  protected void sendError(@Nonnull CommandSender sender, @Nonnull String message) {
+    CommandFeedback.error(sender, message);
+
+  }
+
+  protected void sendError(@Nonnull CommandSender sender, @Nonnull String message,
+      @Nonnull String title) {
+    CommandFeedback.error(sender, message, title);
+  }
+
+  protected void sendWarning(@Nonnull CommandSender sender, @Nonnull String message) {
+    CommandFeedback.warning(sender, message);
+
+  }
+
+  protected void sendWarning(@Nonnull CommandSender sender, @Nonnull String message,
+      @Nonnull String title) {
+    CommandFeedback.warning(sender, message, title);
+  }
+
+  protected void sendInfo(@Nonnull CommandSender sender, @Nonnull String message) {
+    CommandFeedback.info(sender, message);
+
+  }
+
+  protected void sendInfo(@Nonnull CommandSender sender, @Nonnull String message,
+      @Nonnull String title) {
+    CommandFeedback.info(sender, message, title);
+  }
+
+  // Tab completion methods
+  protected List<String> completePlayerName(String prefix) {
+    return CommandTabCompleter.completePlayerName(prefix);
+  }
+
+  protected List<String> completeFromList(String prefix, List<String> options) {
+    return CommandTabCompleter.completeFromList(prefix, options);
+  }
+
+  protected List<String> completeNumbers(String prefix, int min, int max) {
+    return CommandTabCompleter.completeNumbers(prefix, min, max);
+  }
+
+  protected List<String> completeBooleans(String prefix, String format) {
+    return CommandTabCompleter.completeBooleans(prefix, format);
   }
 }
